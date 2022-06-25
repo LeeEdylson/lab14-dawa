ceb5d904c0a55fcf1794d53fe006ea5da9962c40
leeedylson19 <lee.edylson19@gmail.com>
2022-06-25 18:40:36 -0500

    first commit

---
+++ APIS/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace APIS
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{aption}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ APIS/Controllers/PersonsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using API.Models;
+
+
+namespace API.Controllers
+{
+    public class PersonsController : Controller
+    {
+        public List<PersonResponse> Get()
+        {
+            var result = new List<PersonResponse>();
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new PersonResponse {  FullName = "Lee" });
+            }
+            return result;
+        }
+        public PersonResponse Insert(PersonRequest request)
+        {
+            var result = new PersonResponse();
+            result.FullName = string.Concat(request.FirstName, request.LastName);
+            return result;
+        }
+    }
+}
